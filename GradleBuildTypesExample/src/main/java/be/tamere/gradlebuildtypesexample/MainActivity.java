e1810aac230c338d6ae94937d5c2787e566f78e0
Francois Stephany <tulipe.moutarde@gmail.com>
2013-10-05 22:53:22 +0200

    Initial Commit

---
+++ GradleBuildTypesExample/src/main/java/be/tamere/gradlebuildtypesexample/MainActivity.java
@@ -0,0 +1,27 @@
+package be.tamere.gradlebuildtypesexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView serverUrlTextView = (TextView)findViewById(R.id.server_url_text_view);
+        serverUrlTextView.setText(Constants.BASE_URL);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
---
+++ GradleBuildTypesExample/src/production/java/be/tamere/gradlebuildtypesexample/Constants.java
@@ -0,0 +1,5 @@
+package be.tamere.gradlebuildtypesexample;
+
+public class Constants {
+    public final static String BASE_URL = "http://production.tamere.be";
+}
