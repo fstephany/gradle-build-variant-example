25d93e183e512bc39f5d86e4d2640a2746a0c59c
xuexuan <384324069@qq.com>
2016-05-27 11:13:40 +0800

    compiled correctly use Android Studio 2.1.1 and add new ways manifest merge

--- GradleBuildTypesExample/src/main/java/be/tamere/gradlebuildtypesexample/MainActivity.java
+++ GradleBuildTypesExample/src/main/java/be/tamere/gradlebuildtypesexample/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         TextView serverUrlTextView = (TextView)findViewById(R.id.server_url_text_view);
-        serverUrlTextView.setText(Constants.BASE_URL);
+//        serverUrlTextView.setText(Constants.BASE_URL);
     }
 
 
